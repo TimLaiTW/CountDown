1c86e00e4a6a7b0112edcd0e098477f79df3063f
T.Lai <timlai1208@gmail.com>
2021-09-13 10:52:29 +0800

    count down with specific date

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
 
-function App() {
+export default function App() {
+  const now = new Date();
+  const scheduledTime = new Date(2022, 0, 1, 0, 0, 0);
+  const [TimeDiffer, setTimeDiffer] = useState(scheduledTime - new Date());
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTimeDiffer(TimeDiffer - 1000);
+    }, 1000);
+    return () => clearInterval(interval);
+  });
+
+  const time = TimeDiffer / 1000;
+  const dayLeft = Math.floor(time / 60 / 60 / 24);
+  const hourLeft = Math.floor((time / 60 / 60) % 24);
+  const minLeft = Math.floor((time / 60) % 60);
+  const secLeft = Math.floor(time % 60);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>
+        Current time : {now.getFullYear()} / {now.getMonth() + 1} /{" "}
+        {now.getDate()} - {now.getHours()} : {now.getMinutes()} :{" "}
+        {now.getSeconds()}
+      </h2>
+      <h2>
+        Scheduled time : {scheduledTime.getFullYear()} /{" "}
+        {scheduledTime.getMonth() + 1} / {scheduledTime.getDate()} -{" "}
+        {scheduledTime.getHours()} : {scheduledTime.getMinutes()} :{" "}
+        {scheduledTime.getSeconds()}
+      </h2>
+      <h2>
+        Time Left: {dayLeft} - {hourLeft} : {minLeft} : {secLeft}
+      </h2>
     </div>
   );
 }
-
-export default App;
